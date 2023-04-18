45062da4dfe74950f21909bd0414b1757e9c2014
Saad5400 <sdbtwa@gmail.com>
2023-04-18 16:29:30 +0300

    working

--- index.html.js
+++ index.html.js
@@ -44,132 +44,80 @@ const charListMap = {
 function getKey() {
     // get key from input
     const key = input.value;
-    // // if key is empty, return null
-    // if (key === '') {
-    //     return null;
-    // }
     // split key by '-'
     const keyParts = key.split('-');
-    // // if keyParts length is not 2, return null
-    // if (keyParts.length !== 2) {
-    //     return null;
-    // }
     // get first part of key
     const key1 = keyParts[0];
     // get second part of key
     const key2 = keyParts[1];
-    // // if key1 length is not 4, return null
-    // if (key1.length <= 1) {
-    //     return null;
-    // }
-    // // if none of the chars in key1 are in the map, return null
-    // for (let i = 0; i < key1.length; i++) {
-    //     const char = key1[i];
-    //     if (!charListMap[char]) {
-    //         return null;
-    //     }
-    // }
-    // // if key2 length is not 4, return null
-    // if (key2.length <= 1) {
-    //     return null;
-    // }
-    // // if key2 is not a number, return null
-    // if (isNaN(key2)) {
-    //     return null;
-    // }
     // return key1 and key2
     return {
         key1,
         key2
     };
 }
-
+    
 function getInfo() {
     // get key
     const key = getKey();
     key1 = key.key1;
     key2 = key.key2;
-
-    let chars = []
+    
+    let chars = [];
     // loop foreach char in key1
     for (let i = 0; i < key1.length; i++) {
-        // get char
-        const char = key1[i];
-        // get char list from map
-        const charList = charListMap[char];
-        // add char list to chars
-        chars = chars.concat(charList);
+    // get char
+    const char = key1[i];
+    // get char list from map
+    const charList = charListMap[char];
+    // add char list to chars
+    chars = chars.concat(charList);
     }
     // make sure chars distinct
     chars = [...new Set(chars)];
     // while key is longer than chars length, subtract chars length from key
     while (key2.length > chars.length) {
-        key2 -= chars.length;
+    key2 -= chars.length;
     }
-    console.log(chars);
     // return chars and key2
     return {
-        chars,
-        key2
+        "chars": chars,
+        "key": parseInt(key2)
     };
 }
 
 function encrypt(text) {
-    let encryptedText = '';
-    const info = getInfo();
+    let encrypted = "";
+    let info = getInfo();
     const chars = info.chars;
-    const key = info.key2;
-    // loop foreach char in text
+    const key = info.key;
+
     for (let i = 0; i < text.length; i++) {
-        // get char
-        const char = text[i];
-        // get index of char in chars
-        const index = chars.indexOf(char);
-        // if index is -1, add char to encryptedText
-        if (index === -1) {
-            encryptedText += char;
-            continue;
-        }
-        // assign index + key to encryptedTextIndex
-        let encryptedTextIndex = index + key;
-        // while encryptedTextIndex is bigger than chars length, subtract chars length from encryptedTextIndex
-        while (encryptedTextIndex >= chars.length) {
-            encryptedTextIndex -= chars.length;
-        }
-        // add char at encryptedTextIndex to encryptedText
-        encryptedText += chars[encryptedTextIndex];
+      const charIndex = chars.indexOf(text[i]);
+      if (charIndex === -1) {
+        encrypted += text[i];
+      } else {
+        encrypted += chars[(charIndex + key) % chars.length];
+      }
     }
-    // return encryptedText
-    return encryptedText;
+    return encrypted;
 }
 
 function decrypt(text) {
-    let decryptedText = '';
-    const info = getInfo();
+    let decrypted = "";
+    let info = getInfo();
     const chars = info.chars;
-    const key = info.key2;
-    // loop foreach char in text
+    const key = info.key;
+
     for (let i = 0; i < text.length; i++) {
-        // get char
-        const char = text[i];
-        // get index of char in chars
-        const index = chars.indexOf(char);
-        // if index is -1, add char to decryptedText
-        if (index === -1) {
-            decryptedText += char;
-            continue;
+        const charIndex = chars.indexOf(text[i]);
+        if (charIndex === -1) {
+            decrypted += text[i];
+        } else {
+            decrypted += chars[((charIndex - key) % chars.length) + chars.length];
         }
-        // assign index - key to decryptedTextIndex
-        let decryptedTextIndex = index - key;
-        // while decryptedTextIndex is smaller than 0, add chars length to decryptedTextIndex
-        while (decryptedTextIndex < 0) {
-            decryptedTextIndex += chars.length;
-        }
-        // add char at decryptedTextIndex to decryptedText
-        decryptedText += chars[decryptedTextIndex];
-    }
-    // return decryptedText
-    return decryptedText;
+      }
+    return decrypted;
 }
 
 // textarea1 on input
@@ -180,4 +128,16 @@ textarea1.oninput = function () {
 // textarea2 on input
 textarea2.oninput = function () {
     textarea1.value = encrypt(textarea2.value);
+}
+
+function copyInput(id) {
+    const input = document.getElementById(id);
+    navigator.clipboard.writeText(input.value);
+}
+function pasteInput(id) {
+    const input = document.getElementById(id);
+    navigator.clipboard.readText().then(text => {
+        input.value = text;
+        textarea2.value = decrypt(textarea1.value);
+    });
 }
