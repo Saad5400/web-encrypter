e2ce41b411fc476f04dc6369cd1f7a08b1d25233
Saad5400 <sdbtwa@gmail.com>
2023-04-20 01:45:31 +0300

    added gitignore and fixed issues

--- index.html.js
+++ index.html.js
@@ -1,26 +1,31 @@
+function shuffle(array, seed) {
+    let myrng = new Math.seedrandom(seed);
+
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(myrng() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
 // get by id short-input
 const input = document.getElementById('short-input');
 // textarea1
 const textarea1 = document.getElementById('textarea1');
 // textarea2
 const textarea2 = document.getElementById('textarea2');
-
-function setRandomKey() {
-    // generate word that contains 4 letters, each are distinct, each letter can only be from A,E,R,P
-    let randomKey = [...Array(5)].map(() => 'AERPJ'[Math.floor(Math.random() * 5)]).join('');
-    // make sure randomKey has at least one 'A' and one 'E'
-    if (!randomKey.includes('A') || !randomKey.includes('E')) {
-        return setRandomKey();
-    }
-    if (randomKey.includes("RAPE")) {
-        return setRandomKey();
-    }
-    // add a number of 4 digits to the end of the randomKey after a '-'
-    randomKey += '-' + Math.floor(Math.random() * 10000).toString().padStart(4, '0');
-    // set random key to input
-    input.value = randomKey;
-}
-setRandomKey();
+// last input
+let lastInput = 1;
 
 // list of all ascii chars
 const asciiChars = [...Array(256)].map((_, i) => String.fromCharCode(i));
@@ -50,18 +55,26 @@ function getKey() {
     const key1 = keyParts[0];
     // get second part of key
     const key2 = keyParts[1];
-    // return key1 and key2
+
+    let key3 = "default";
+    if (keyParts.length >= 3) {
+        key3 = keyParts[2];
+    }
+
+    // return key1 and key2 and key3
     return {
         key1,
-        key2
+        key2,
+        key3
     };
 }
     
 function getInfo() {
     // get key
-    const key = getKey();
-    key1 = key.key1;
-    key2 = key.key2;
+    const key = getKey(),
+    key1 = key.key1,
+    key2 = key.key2,
+    key3 = key.key3;
     
     let chars = [];
     // loop foreach char in key1
@@ -79,6 +92,7 @@ function getInfo() {
     while (key2.length > chars.length) {
     key2 -= chars.length;
     }
+    chars = shuffle(chars, key3);
     // return chars and key2
     return {
         "chars": chars,
@@ -123,11 +137,23 @@ function decrypt(text) {
 // textarea1 on input
 textarea1.oninput = function () {
     textarea2.value = decrypt(textarea1.value);
+    lastInput = 1;
 }
 
 // textarea2 on input
 textarea2.oninput = function () {
     textarea1.value = encrypt(textarea2.value);
+    lastInput = 2;
+}
+
+input.oninput = function () {
+    if (lastInput === 1) {
+        textarea2.value = decrypt(textarea1.value);
+    } 
+    else {
+        textarea1.value = encrypt(textarea2.value);
+    }
+    localStorage.setItem('key', input.value);
 }
 
 function copyInput(id) {
@@ -140,4 +166,42 @@ function pasteInput(id) {
         input.value = text;
         textarea2.value = decrypt(textarea1.value);
     });
+}
+
+function setRandomKey() {
+    // generate word that contains 4 letters, each are distinct, each letter can only be from A,E,R,P
+    let randomKey = [...Array(20)].map(() => 'AERPJ'[Math.floor(Math.random() * 5)]).join('');
+    // make sure randomKey is distinct
+    randomKey = [...new Set(randomKey)].join('');
+    // make sure randomKey has at least one 'A' and one 'E'
+    if (!randomKey.includes('A') || !randomKey.includes('E')) {
+        return setRandomKey();
+    }
+    // add a number of 4 digits to the end of the randomKey after a '-'
+    randomKey += '-' + Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+    // get a random word of 8 letters and or numbers
+    const randomKey2 = [...Array(8)].map(() => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'[Math.floor(Math.random() * 62)]).join('');
+    // add randomKey2 to randomKey after a '-'
+    randomKey += '-' + randomKey2;
+    // set random key to input
+    input.value = randomKey;
+
+    if (lastInput === 1) {
+        textarea2.value = decrypt(textarea1.value);
+    } else {
+        textarea1.value = encrypt(textarea2.value);
+    }
+
+    localStorage.setItem('key', input.value);
+}
+// check if there is a value for the key in local storage
+if (localStorage.getItem('key')) {
+    // set the value of the key in local storage to input
+    input.value = localStorage.getItem('key');
+    // paste to textarea1
+    pasteInput('textarea1');
+}
+else {
+    // set random key
+    setRandomKey();
 }
