95e0cb416273fb392194d6e01428917b650d3467
Saad5400 <sdbtwa@gmail.com>
2023-05-12 23:19:03 +0300

    ..

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,61 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
-import { startEncrpyer } from './encrypter';
+import { useEffect, useState, useRef } from 'react';
+import { encrypt, decrypt, getRandomKey } from './encrypter';
 
 function Link(props) {
 	return (<a className='link link-hover link-primary' onClick={props.onClick}>{props.text}</a>);
 }
 
 function Input(props) {
+
+	function onChange(event) {
+		props.setValue(event.target.value);
+		localStorage.setItem('key', event.target.value);
+	}
+
+	function generateKey() {
+		let randomKey = getRandomKey();
+		props.setValue(randomKey);
+		localStorage.setItem('key', randomKey);
+	}
+
 	return (
 		<div className="form-control Responsive-width">
 			<label className="label">
 				<b className="label-text">Key</b>
 				<span className="label-text-alt">
-					<Link text='Generate' />
+					<Link text='Generate' onClick={generateKey} />
 				</span>
 			</label>
-			<input id='short-input' type="text" placeholder="Type here" className="input input-sm md:input-md input-bordered Responsive-width" />
+			<input value={props.value} onChange={onChange} type="text" placeholder="Type here" className="input input-sm md:input-md input-bordered Responsive-width" />
 		</div>
 	);
 }
 
 function TextArea(props) {
+
+	function onChangePlain(event) {
+		props.setLastInput(0);
+		props.setPlainText(event.target.value);
+		props.setEncryptedText(encrypt(event.target.value, props.ekey));
+	}
+	function onChangeEncrypted(event) {
+		props.setLastInput(1);
+		props.setEncryptedText(event.target.value);
+		props.setPlainText(decrypt(event.target.value, props.ekey));
+	}
+
 	return (<div className='Responsive-width'>
 		<div className="form-control">
 			<label className="label">
 				<b className="label-text">{props.title}</b>
 				<span className="label-text-alt">
 					<div>
-						<Link text='Copy ' />
-						<Link text='Paste' />
+						<Link text='Copy ' onClick={() => {
+							navigator.clipboard.writeText(props.title == "Plain Text" ? props.plainText : props.encryptedText);
+						}} />
+						<Link text='Paste' onClick={() => {
+							navigator.clipboard.readText().then(text => {
+								if (props.title == "Plain Text") {
+									props.setPlainText(text);
+									props.setEncryptedText(encrypt(text, props.ekey));
+								} else {
+									props.setEncryptedText(text);
+									props.setPlainText(decrypt(text, props.ekey));
+								}
+							});
+						}} />
 					</div>
 				</span>
 			</label>
-			<textarea id={props.id} className="textarea textarea-bordered h-40 leading-normal" placeholder={"Enter your " + props.title.toLowerCase() + " here"}></textarea>
+			<textarea onChange={props.title == "Plain Text" ? onChangePlain : onChangeEncrypted} className="textarea textarea-bordered h-40 leading-normal" placeholder={"Enter your " + props.title.toLowerCase() + " here"} value={props.title == "Plain Text" ? props.plainText : props.encryptedText}></textarea>
 		</div>
 	</div>);
-}
-
-
-function Modal() {
-	return (
-		<div>
-			<input type="checkbox" id="my-modal-3" className="modal-toggle" />
-			<div className="modal">
-				<div className="modal-box relative">
-					<label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-					<p id='specialModal' className="py-4"></p>
-					<div className="modal-action">
-						<button className="btn" id='modalPrev'>Previous</button>
-						<button className="btn" id='modalNext'>Next</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-
 }
 
 function Navbar() {
@@ -84,19 +100,33 @@ function Card(props) {
 }
 
 function App() {
+
+	const [firstRender, setFirstRender] = useState(true);
+
+	const [key, setKey] = useState(getRandomKey());
+	const [lastInput, setLastInput] = useState(0);
+	const [plainText, setPlainText] = useState("");
+	const [encryptedText, setEncryptedText] = useState("");
+
 	useEffect(() => {
-		startEncrpyer();
-	}, []);
+		if (firstRender) {
+			// Perform the action only on the first render
+			let storedKey = localStorage.getItem('key');
+			if (storedKey) {
+				setKey(storedKey);
+			}
+			setFirstRender(false);
+		}
+	}, [firstRender, key, plainText, encryptedText, lastInput]);
+
 	return (
 		<div className="App">
 			<Navbar />
-			<Modal />
-			<label id='specialModalBtn' htmlFor="my-modal-3" className="btn hidden">open modal</label>
 			<Card>
-				<Input />
+				<Input value={key} setValue={setKey} />
 				<div className='h-1'></div>
-				<TextArea id="textarea2" title='Plain Text' />
-				<TextArea id="textarea1" title='Encrypted Text' />
+				<TextArea title='Plain Text' lastInput={lastInput} setLastInput={setLastInput} plainText={plainText} setPlainText={setPlainText} encryptedText={encryptedText} setEncryptedText={setEncryptedText} ekey={key} />
+				<TextArea title='Encrypted Text' lastInput={lastInput} setLastInput={setLastInput} plainText={plainText} setPlainText={setPlainText} encryptedText={encryptedText} setEncryptedText={setEncryptedText} ekey={key} />
 			</Card>
 		</div>
 	);
--- my-project/src/encrypter.js
+++ my-project/src/encrypter.js
@@ -1,41 +1,5 @@
 import { Base64 } from 'js-base64';
 let seedrandom = require('seedrandom');
-let index = 0;
-
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function explode(event) {
-    console.log("Exploding char");
-    for (let i = 0; i < 20; i++) {
-        const heart = document.createElement('div');
-        heart.classList.add('heart');
-        heart.innerHTML = '🤍';
-        heart.style.animationDelay = `${getRandom(0, 3)}s`;
-        heart.style.animationDuration = `${getRandom(3, 6)}s`;
-
-        heart.style.position = 'absolute';
-        let variance = 500;
-        heart.style.left = event.clientX + getRandom(-variance, variance) + 'px';
-        heart.style.top = event.clientY + getRandom(-variance, variance) + 'px';
-        heart.style.zIndex = 9999;
-        heart.hasPointerCapture = false;
-
-        // if heart is outside the screen from left or right
-        document.body.appendChild(heart);
-        if (heart.style.left < 0 || heart.getBoundingClientRect().right > window.screen.width) {
-            heart.remove();
-        }
-
-        setInterval(() => {
-            // if heart is out of screen, remove it
-            if (heart.getBoundingClientRect().top < 0) {
-                heart.remove();
-            }
-        }, 1000);
-    }
-}
 
 function shuffle(array, seed) {
     let myrng = new seedrandom(seed);
@@ -76,197 +40,141 @@ const charListMap = {
     'J': japaneseChars
 };
 
-let input, textarea1, textarea2, lastInput;
-function startEncrpyer() {
+function getKey(key) {
+    // split key by '-'
+    const keyParts = key.split('-');
+    // get first part of key
+    const key1 = keyParts[0];
+    // get second part of key
+    const key2 = keyParts[1];
 
-    // get by id short-input
-    input = document.getElementById('short-input');
-    // textarea1
-    textarea1 = document.getElementById('textarea1');
-    // textarea2
-    textarea2 = document.getElementById('textarea2');
-    // last input
-    lastInput = 1;
-
-
-    function getKey() {
-        // get key from input
-        const key = input.value;
-        // split key by '-'
-        const keyParts = key.split('-');
-        // get first part of key
-        const key1 = keyParts[0];
-        // get second part of key
-        const key2 = keyParts[1];
-
-        let key3 = "default";
-        if (keyParts.length >= 3) {
-            key3 = keyParts[2];
-        }
-
-        // return key1 and key2 and key3
-        return {
-            key1,
-            key2,
-            key3
-        };
+    let key3 = "default";
+    if (keyParts.length >= 3) {
+        key3 = keyParts[2];
     }
+    // return key1 and key2 and key3
+    return {
+        key1,
+        key2,
+        key3
+    };
+}
+function getInfo(str) {
+    // get key
+    let key = getKey(str),
+        key1 = key.key1,
+        key2 = key.key2,
+        key3 = key.key3;
 
-    function getInfo() {
-        // get key
-        let key = getKey(),
-            key1 = key.key1,
-            key2 = key.key2,
-            key3 = key.key3;
-
-        let chars = [];
-        // loop foreach char in key1
-        for (let i = 0; i < key1.length; i++) {
-            // get char
-            const char = key1[i];
-            // get char list from map
-            const charList = charListMap[char];
-            // add char list to chars
-            chars = chars.concat(charList);
-        }
-        // make sure chars distinct
-        chars = [...new Set(chars)];
-        // while key is longer than chars length, subtract chars length from key
-        while (key2.length > chars.length) {
-            key2 -= chars.length;
-        }
-        chars = shuffle(chars, key3);
-        // return chars and key2
-        return {
-            "chars": chars,
-            "key": parseInt(key2)
-        };
+    let chars = [];
+    // loop foreach char in key1
+    for (let i = 0; i < key1.length; i++) {
+        // get char
+        const char = key1[i];
+        // get char list from map
+        const charList = charListMap[char];
+        // add char list to chars
+        chars = chars.concat(charList);
     }
-
-    function encrypt(text) {
-        let encrypted = "";
-        let info = getInfo();
-        const chars = info.chars;
-        const key = info.key;
-
-        for (let i = 0; i < text.length; i++) {
-            const charIndex = chars.indexOf(text[i]);
-            if (charIndex === -1) {
-                encrypted += text[i];
-            } else {
-                encrypted += chars[(charIndex + key) % chars.length];
-            }
-        }
-        // convert encrypted to base64
-        encrypted = Base64.encode(encrypted);
-        return encrypted;
+    // make sure chars distinct
+    chars = [...new Set(chars)];
+    // while key is longer than chars length, subtract chars length from key
+    while (key2.length > chars.length) {
+        key2 -= chars.length;
     }
+    chars = shuffle(chars, key3);
+    // return chars and key2
+    return {
+        "chars": chars,
+        "key": parseInt(key2)
+    };
+}
 
-    function decrypt(text) {
+function encrypt(text, _key) {
+    let encrypted = "";
+    let info = getInfo(_key);
+    const chars = info.chars;
+    const key = info.key;
+
+    for (let i = 0; i < text.length; i++) {
+        const charIndex = chars.indexOf(text[i]);
+        if (charIndex === -1) {
+            encrypted += text[i];
+        } else {
+            encrypted += chars[(charIndex + key) % chars.length];
+        }
+    }
+    // convert encrypted to base64
+    encrypted = Base64.encode(encrypted);
+    return encrypted;
+}
+
+function decrypt(text, _key) {
+    try {
         text = Base64.decode(text);
-        let decrypted = "";
-        let info = getInfo();
-        const chars = info.chars;
-        const key = info.key;
+    } catch (error) {
 
-        for (let i = 0; i < text.length; i++) {
-            const charIndex = chars.indexOf(text[i]);
-            if (charIndex === -1) {
-                decrypted += text[i];
-            } else {
-                let ind = ((charIndex - key) % chars.length);
-                if (ind < 0) { ind += chars.length; }
-                decrypted += chars[ind];
-            }
+    }
+    let decrypted = "";
+    let info = getInfo(_key);
+    const chars = info.chars;
+    const key = info.key;
+
+    for (let i = 0; i < text.length; i++) {
+        const charIndex = chars.indexOf(text[i]);
+        if (charIndex === -1) {
+            decrypted += text[i];
+        } else {
+            let ind = ((charIndex - key) % chars.length);
+            if (ind < 0) { ind += chars.length; }
+            decrypted += chars[ind];
         }
-        return decrypted;
     }
+    return decrypted;
+}
 
-    // textarea1 on input
-    textarea1.oninput = function () {
-        textarea2.value = decrypt(textarea1.value);
-        // if value contains a strign
-        if (textarea2.value.toLowerCase().includes("special")) {
-            fetch(`https://gptapi5400.azurewebsites.net/Special/Get?text=${textarea2.value}&key=${input.value}}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    const specialModalBtn = document.getElementById('specialModalBtn');
-                    specialModalBtn.click();
-                    const specialModal = document.getElementById('specialModal');
-                    const modalNext = document.getElementById('modalNext');
-                    const modalPrev = document.getElementById('modalPrev');
+    // // textarea1 on input
+    // textarea1.oninput = function () {
+    //     textarea2.value = decrypt(textarea1.value);
+    //     lastInput = 1;
+    // }
 
-                    specialModal.innerHTML = data.data[index].text;
-                    modalNext.onclick = function () {
-                        console.log(data.data);
-                        if (index < data.data.length - 1) {
-                            index++;
-                        }
-                        specialModal.innerHTML = data.data[index].text;
-                        if (index == 4) {
-                            document.addEventListener("click", explode);
-                        }
-                        if (index == 6) {
-                            document.removeEventListener("click", explode);
-                        }
-                        if (index == 18) {
-                            document.addEventListener("click", explode);
-                        }
-                    }
-                    modalPrev.onclick = function () {
-                        if (index > 0) {
-                            index--;
-                        }
-                        specialModal.innerHTML = data.data[index].text;
-                        if (index == 6) {
-                            document.addEventListener("click", explode);
-                        }
-                        if (index == 17) {
-                            document.removeEventListener("click", explode);
-                        }
-                    }
-                });
-        }
-        lastInput = 1;
-    }
+    // // textarea2 on input
+    // textarea2.oninput = function () {
+    //     textarea1.value = encrypt(textarea2.value);
+    //     lastInput = 2;
+    // }
 
-    // textarea2 on input
-    textarea2.oninput = function () {
-        textarea1.value = encrypt(textarea2.value);
-        lastInput = 2;
-    }
+    // input.oninput = function () {
+    //     if (lastInput === 1) {
+    //         textarea2.value = decrypt(textarea1.value);
+    //     }
+    //     else {
+    //         textarea1.value = encrypt(textarea2.value);
+    //     }
+    //     localStorage.setItem('key', input.value);
+    // }
 
-    input.oninput = function () {
-        if (lastInput === 1) {
-            textarea2.value = decrypt(textarea1.value);
-        }
-        else {
-            textarea1.value = encrypt(textarea2.value);
-        }
-        localStorage.setItem('key', input.value);
-    }
+    // function copyInput(id) {
+    //     const input = document.getElementById(id);
+    //     navigator.clipboard.writeText(input.value);
+    // }
+    // function pasteInput(id) {
+    //     const input = document.getElementById(id);
+    //     navigator.clipboard.readText().then(text => {
+    //         input.value = text;
+    //         textarea2.value = decrypt(textarea1.value);
+    //     });
+    // }
 
-    function copyInput(id) {
-        const input = document.getElementById(id);
-        navigator.clipboard.writeText(input.value);
-    }
-    function pasteInput(id) {
-        const input = document.getElementById(id);
-        navigator.clipboard.readText().then(text => {
-            input.value = text;
-            textarea2.value = decrypt(textarea1.value);
-        });
-    }
-
-    function setRandomKey() {
+    function getRandomKey() {
         // generate word that contains 4 letters, each are distinct, each letter can only be from A,E,R,P
         let randomKey = [...Array(20)].map(() => 'AERPJ'[Math.floor(Math.random() * 5)]).join('');
         // make sure randomKey is distinct
         randomKey = [...new Set(randomKey)].join('');
         // make sure randomKey has at least one 'A' and one 'E'
         if (!randomKey.includes('A') || !randomKey.includes('E')) {
-            return setRandomKey();
+            return getRandomKey();
         }
         // add a number of 4 digits to the end of the randomKey after a '-'
         randomKey += '-' + Math.floor(Math.random() * 10000).toString().padStart(4, '0');
@@ -275,25 +183,16 @@ function startEncrpyer() {
         // add randomKey2 to randomKey after a '-'
         randomKey += '-' + randomKey2;
         // set random key to input
-        input.value = randomKey;
-
-        if (lastInput === 1) {
-            textarea2.value = decrypt(textarea1.value);
-        } else {
-            textarea1.value = encrypt(textarea2.value);
-        }
-
-        localStorage.setItem('key', input.value);
+        return randomKey;
     }
-    // check if there is a value for the key in local storage
-    if (localStorage.getItem('key')) {
-        // set the value of the key in local storage to input
-        input.value = localStorage.getItem('key');
-    }
-    else {
-        // set random key
-        setRandomKey();
-    }
-}
+    // // check if there is a value for the key in local storage
+    // if (localStorage.getItem('key')) {
+    //     // set the value of the key in local storage to input
+    //     input.value = localStorage.getItem('key');
+    // }
+    // else {
+    //     // set random key
+    //     setRandomKey();
+    // }
 
-export { startEncrpyer };
+export { encrypt, decrypt, getRandomKey }
